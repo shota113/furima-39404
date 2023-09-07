c20945cf9493ce527c938075852522e52c8f00a5
shota113 <neverlose113@icloud.com>
2023-09-07 21:50:40 +0900

    ストロングパラメーター(新規登録)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def user_params
+    params.require(:user).paermit(:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_day)
 end
