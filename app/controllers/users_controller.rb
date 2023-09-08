94d00dc7e286fd18c6f11ef38af11d869be109a0
shota113 <neverlose113@icloud.com>
2023-09-08 21:36:02 +0900

    ログイン有無の表示の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).paermit(:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_day)
+    params.require(:user).permit(:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_day)
+  end
 end
