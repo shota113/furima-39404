1b6012a91c6ba0f0e8c47f38b8f5ce7fa4f3f277
shota113 <neverlose113@icloud.com>
2023-09-15 22:24:57 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :buyer
+  belongs_to :user_id
+  belongs_to :category_id
+  belongs_to :cost_id
+  belongs_to :prefecture_id
+  belongs_to :shipment_id
+
+  validates :name, presence: true
+  validates :user, presence: true,foreign_key: true
+  validates :price, presence: true
+  validates :content, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipment_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :cost_id, presence: true
 end
