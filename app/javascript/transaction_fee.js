c675a60ddbf46bf37454bae08da92de225fa4f09
shota113 <neverlose113@icloud.com>
2023-09-23 20:13:04 +0900

    Update transaction_fee.js

--- app/javascript/transaction_fee.js
+++ app/javascript/transaction_fee.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitInput = document.getElementById("profit");
+  addTaxDom.innerHTML = Math.floor(inputValue*0.1);
+  profitInput.innerHTML = inputValue - addTaxDom.innerHTML;
+    console.log(inputValue);
+  });
 });
