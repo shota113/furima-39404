b4d0afbef6383c85c7751b8da9adedc572aef3a3
shota113 <neverlose113@icloud.com>
2023-09-24 15:53:06 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,12 +15,12 @@ class ItemsController < ApplicationController
     end
   end
   def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to item_path(@item)
-    else
-      # render 'edit'
-    end
+  #   @item = Item.find(params[:id])
+  #   if @item.update(item_params)
+  #     redirect_to item_path(@item)
+  #   else
+  #      render 'edit'
+  #   end
   end
 
   private
