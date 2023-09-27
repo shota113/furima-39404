7b17f211cda76aa91d35899966a369f371733be0
shota113 <neverlose113@icloud.com>
2023-09-27 19:34:39 +0900

    編集の遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,12 +16,12 @@ class ItemsController < ApplicationController
     end
   end
   def update
-  #   @item = Item.find(params[:id])
-  #   if @item.update(item_params)
-  #     redirect_to item_path(@item)
-  #   else
-  #      render 'edit'
-  #   end
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+       render 'edit'
+    end
   end
   # def edit
   #   @item = Item.find(params[:id])
