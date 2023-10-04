dad184f6718795caba840c116928c8619b0c1eb1
shota113 <neverlose113@icloud.com>
2023-10-04 16:44:57 +0900

    db修正

--- db/migrate/20231002103556_create_addresses.rb
+++ db/migrate/20231002103556_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.references :order, null: false, foreign_key: true
+      t.integer :prefecture_id, null: false
+      t.string :postcode, null: false
+      t.string :city, null: false
+      t.string :street_number, null: false
+      t.string :building_name
+      t.string :tel, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,16 @@ ActiveRecord::Schema.define(version: 2023_10_02_103556) do
   end
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.integer "prefecture_id", null: false
+    t.string "postcode", null: false
+    t.string "city", null: false
+    t.string "street_number", null: false
+    t.string "building_name"
+    t.string "tel", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_addresses_on_order_id"
   end
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -64,8 +72,12 @@ ActiveRecord::Schema.define(version: 2023_10_02_103556) do
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -87,5 +99,8 @@ ActiveRecord::Schema.define(version: 2023_10_02_103556) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "orders"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
 end
