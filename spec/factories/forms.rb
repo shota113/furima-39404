5c59386a513abe0c34d26ccb365971379c0f3efc
shota113 <neverlose113@icloud.com>
2023-10-13 20:55:35 +0900

    payjpの導入

--- spec/factories/forms.rb
+++ spec/factories/forms.rb
@@ -1,11 +1,12 @@
 FactoryBot.define do
   factory :form do
 
-
-    postcode         {"123-4567"}
+    item {1}
+    user_id {1}
+    postcode         {Faker::Number.between(from: 001, to: 999).to_s + "-" + Faker::Number.between(from: 0001, to: 9999).to_s}
     prefecture_id    {Faker::Number.between(from: 2, to: 48)}
     city             {Faker::Number.between(from: 300, to: 9_999_999)}
-    street_number    {"坂道1-13"}
+    street_number    {"坂道4-2"}
     building_name    {Faker::Internet.name}
     tel              {"12345678912"}
   end
