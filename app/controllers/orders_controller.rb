928a99b8e9d6c3cba715a2fa635f320eadb87f31
shota113 <neverlose113@icloud.com>
2023-10-14 20:56:16 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,8 @@ class OrdersController < ApplicationController
     @form = Form.new
     @item = Item.find(params[:item_id])
 
-    if user_signed_in? && current_user.id != @item.user_id
-     
+    if user_signed_in? && current_user.id == @item.order.nil?
+     render 'index'
     else
       redirect_to root_path
     end
