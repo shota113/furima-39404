1333da557132de6de00babc9bc1a2b68612323c8
shota113 <neverlose113@icloud.com>
2023-10-14 22:57:08 +0900

    修正

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,18 +1,21 @@
 class Form
   include ActiveModel::Model
   attr_accessor :user_id, :item, :postcode, :prefecture_id, :city, :street_number, :building_name, :tel, :token
-  validates :user_id, presence: true
-  validates :item, presence: true
+  with_options presence: true do
+    validates :user_id, presence: true
+    validates :item, presence: true
 
+    validates :postcode, presence: true, format:{with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :prefecture_id, presence: true, numericality:{ other_than: 1 }
+    validates :city, presence: true
+    validates :street_number, presence: true
+    validates :tel, presence: true, format:{with: /\A[0-9]{10,11}\z/}
 
-  validates :postcode, presence: true, format:{with: /\A[0-9]{3}-[0-9]{4}\z/}
-  validates :prefecture_id, presence: true, numericality:{ other_than: 1 }
-  validates :city, presence: true
-  validates :street_number, presence: true
-  validates :tel, presence: true, format:{with: /\A[0-9]{10,11}\z/}
-  validates :token, presence: true
+    validates :token, presence: true
+  end
+
   def save
     order = Order.create(user_id: user_id, item_id: item)
-    
+    Address.create(order_id:order.id, postcode: postcode, prefecture_id: prefecture_id, city: city, street_number: street_number, building_name: building_name, tel: tel )
   end
 end
