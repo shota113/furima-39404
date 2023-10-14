5870c6c5177c3039be8b2068c20be64f4f625fb1
shota113 <neverlose113@icloud.com>
2023-10-14 21:28:53 +0900

    テストコードの修正

--- spec/factories/forms.rb
+++ spec/factories/forms.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
 
     item {1}
     user_id {1}
-    postcode         {Faker::Number.between(from: 001, to: 999).to_s + "-" + Faker::Number.between(from: 0001, to: 9999).to_s}
+    postcode         {Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
     prefecture_id    {Faker::Number.between(from: 2, to: 48)}
     city             {Faker::Number.between(from: 300, to: 9_999_999)}
     street_number    {"坂道4-2"}
